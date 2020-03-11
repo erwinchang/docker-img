a80536553071322f27fcde76d519814c49091748
erwin.chang <m9207216@mail.ntust.edu.tw>
2020-03-11 16:17:25 +0800

    add gitconfig core setting

--- utils/aosp_bashrc.sh
+++ utils/aosp_bashrc.sh
@@ -12,7 +12,6 @@ echo ""
 #unable to access 'https://gerrit.googlesource.com/git-repo/': Problem with the SSL CA cert
 git config --global --add http.sslVerify false
 
-#./build-core-sdk.sh dra7xx-evm
 #Please use a locale setting which supports UTF-8 (such as LANG=en_US.UTF-8).
 export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
